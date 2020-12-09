69c1918d010e995f76244e5b2013c909f347cde2
gelu4102 <sabukuk@gmail.com>
2020-12-08 18:34:19 -0600

    initial commit

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    public void test(){
+        Assertions.assertEquals(4,3+1);
+    }
+
+
+}
