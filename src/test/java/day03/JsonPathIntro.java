2601919b390eb7744f680a0d4f3ca68b3ad054a8
gelu4102 <sabukuk@gmail.com>
2020-12-10 19:37:13 -0600

    Json PathIntro Extracting value

--- src/test/java/day03/JsonPathIntro.java
+++ src/test/java/day03/JsonPathIntro.java
@@ -1,6 +1,7 @@
 package day03;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -29,6 +30,33 @@ public class JsonPathIntro {
     @DisplayName("Extracting data out of Spartan Json Object")
     @Test
     public void test1SpartanPayload(){
+
+        //send a request to get 1 spartan
+        //by providing path params with valid id
+        //save it into Response object
+        //NEW : create an object with type JsonPath
+        //by calling the method jsonPath() on response object
+        //extract id, name, gender, phone
+        //and save iyt into variable of correct type
+
+     Response response =   given()
+                                  .pathParam("id", 34).
+                           when()
+                                  .get("/spartans/{id}")
+                                  .prettyPeek()
+                                      ;
+     //response.prettyPrint();
+     //JsonPath is used to navigate through the json payload
+        //and extract the value according to the valid "jsonpath" provided
+        JsonPath jp = response.jsonPath();
+        int myId = jp.getInt("id");
+        String myName = jp.getString("name");
+        String gender = jp.getString("gender");
+        long phone = jp.getLong("phone");
+        System.out.println("myId = " + myId);
+        System.out.println("myName = " + myName);
+        System.out.println("myGender = " + gender);
+        System.out.println("myPhone = " + phone);
 
     }
 
